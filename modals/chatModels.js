8e26ed35bc79cf8541ec9ecdd2e506fe12469f97
khalad007 <www.arifhossain49919@gmail.com>
2023-12-31 09:22:27 +0600

    Created chat,message model & schema

---
+++ modals/chatModels.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const chatModels = mongoose.Schema({
+    chatName: { type: String },
+    isGroupChat: { type: Boolean },
+    users: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    latestMessage: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message"
+    },
+    groupAdmin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+})
+
+const Chat = mongoose.Model("Chat", chatModels)
+module.exports = Chat;
